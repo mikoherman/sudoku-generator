9c3197809f64b2e723bb9a968714d89cdbb256cd
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 20:17:52 +0200

    add interface to abstract PDF creation for puzzles and their solutions

---
+++ SudokuGenerator/FileHandling/IPdfFileProcessor.cs
@@ -0,0 +1,8 @@
+﻿using SudokuGenerator.Models;
+
+namespace Sudoku_Generator.FileHandling;
+
+public interface IPdfFileProcessor
+{
+    Task CreatePdfsFromSudokusAsync(string solvableFileName, string solutionsFileName, IEnumerable<Sudoku> sudokus);
+}
