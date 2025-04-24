62bd099d612abeae443174e6bd1949d7aa775812
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 20:17:04 +0200

    add PdfFileProcessor for creating PDFs from puzzles and solutions

---
+++ SudokuGenerator/FileHandling/PdfFileProcessor.cs
@@ -0,0 +1,38 @@
+﻿using Sudoku_Generator.Events;
+using SudokuGenerator.Models;
+
+namespace Sudoku_Generator.FileHandling;
+
+public class PdfFileProcessor : IPdfFileProcessor, IProcessNotifier
+{
+    private readonly ISudokuPdfHandler _pdfHandler;
+    public event EventHandler<IsProcessFinishedEventArgs>? ProcessFinished;
+
+    public PdfFileProcessor(ISudokuPdfHandler pdfHandler)
+    {
+        _pdfHandler = pdfHandler;
+    }
+
+    public async Task CreatePdfsFromSudokusAsync(string solvableFileName, string solutionsFileName, IEnumerable<Sudoku> sudokus)
+    {
+        var tasks = new List<Task> {
+        Task.Run(() =>
+        _pdfHandler.CreatePdf(
+            solvableFileName,
+            "Sudoku",
+            sudokus.Select(sudoku => sudoku.SolvableBoard))),
+        Task.Run(() =>
+        _pdfHandler.CreatePdf(
+            solutionsFileName,
+            "Solutions",
+            sudokus.Select(sudoku => sudoku.Solution)))
+        };
+        await Task.WhenAll(tasks);
+        OnProcessFinished();
+    }
+
+    private void OnProcessFinished() =>
+        ProcessFinished?.Invoke(
+            this,
+            new IsProcessFinishedEventArgs(true));
+}
