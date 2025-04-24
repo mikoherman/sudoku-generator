67f9768062f496af5047fd0c02f3913554d5f173
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 20:47:15 +0200

    add interface to abstract LoopingStatusPrinter

---
+++ SudokuGenerator/UserInteraction/ILoopingStatusPrinter.cs
@@ -0,0 +1,9 @@
+﻿using Sudoku_Generator.Events;
+
+namespace Sudoku_Generator.UserInteraction;
+
+public interface ILoopingStatusPrinter
+{
+    void PrintMessageUponCompletion(string messageBase, string messageUponCompletion, int delayMs);
+    void FinishWork(object? sender, IsProcessFinishedEventArgs eventArgs);
+}
