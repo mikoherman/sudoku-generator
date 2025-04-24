db2c305d040698e136bda50a31fb92c06d971690
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 20:29:36 +0200

    add SudokuGeneratorFactory for easier creation of ISudokuGenerator

---
+++ SudokuGenerator/Factories/SudokuGeneratorFactory.cs
@@ -0,0 +1,27 @@
+﻿using SudokuGenerator.Models;
+using SudokuGenerator.RemovalPatterns;
+using Generators = SudokuGenerator.Generators;
+using SudokuGenerator.Generators;
+
+namespace Sudoku_Generator.Factories;
+
+public class SudokuGeneratorFactory : ISudokuGeneratorFactory
+{
+    private readonly ISudokuBoardFiller _boardFiller;
+    private readonly IRemovalPatternsFactory _removalPatternsFactory;
+    private readonly Random _rand;
+
+    public SudokuGeneratorFactory(ISudokuBoardFiller boardFiller, IRemovalPatternsFactory removalPatternsFactory, Random rand)
+    {
+        _boardFiller = boardFiller;
+        _removalPatternsFactory = removalPatternsFactory;
+        _rand = rand;
+    }
+
+    public ISudokuGenerator CreateSudokuGeneratorFor(Difficulty difficulty)
+    {
+        IList<IRemovalPattern> removalPatterns = _removalPatternsFactory.GetRemovalPatternsFor(difficulty);
+        var generator = new Generators.SudokuGenerator(_boardFiller, removalPatterns, _rand);
+        return generator;
+    }
+}
