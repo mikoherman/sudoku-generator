133e60dbfdd022a1164cd0590789ce9cf3cbef5b
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 11:57:59 +0200

    add SudokuGenerator for generating puzzles using IRemovalPattern

---
+++ SudokuGenerator/Generators/SudokuGenerator.cs
@@ -0,0 +1,35 @@
+﻿using SudokuGenerator.Models;
+using SudokuGenerator.RemovalPatterns;
+
+namespace SudokuGenerator.Generators;
+
+public class SudokuGenerator : ISudokuGenerator
+{
+    private readonly ISudokuBoardFiller _boardFiller;
+    private readonly IList<IRemovalPattern> _removalPatterns;
+    private readonly Random _rand;
+
+    public SudokuGenerator(ISudokuBoardFiller boardFiller,
+        IList<IRemovalPattern> removalPatterns, Random rand)
+    {
+        _boardFiller = boardFiller;
+        _removalPatterns = removalPatterns;
+        _rand = rand;
+    }
+
+    public async Task<IEnumerable<Sudoku>> GenerateBoards(int boardCount)
+    {
+        var tasks = new List<Task<Sudoku>>(boardCount);
+        while (boardCount > 0)
+        {
+            tasks.Add(Task.Run(() =>
+            {
+                int[,] sudokuBoard = _boardFiller.GenerateValidSudokuGrid();
+                IRemovalPattern removalPattern = _removalPatterns[_rand.Next(_removalPatterns.Count())];
+                return removalPattern.ConvertBoardToSudoku(sudokuBoard);
+            }));
+            boardCount--;
+        }
+        return await Task.WhenAll(tasks);
+    }
+}
