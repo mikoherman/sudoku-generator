e34e1468c1e62d06cbe9324769550f4bf887794e
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 12:28:48 +0200

    add DifficultyUtils for working with Difficulty enum

---
+++ SudokuGenerator/Utilities/DifficultyUtils.cs
@@ -0,0 +1,19 @@
+﻿using SudokuGenerator.Models;
+
+namespace Sudoku_Generator.Utilities;
+
+public static class DifficultyUtils
+{
+    public static bool TryParse(int number, out Difficulty difficulty)
+    {
+        if (Enum.IsDefined(typeof(Difficulty), number))
+        {
+            difficulty = (Difficulty)number;
+            return true;
+        }
+        difficulty = default;
+        return false;
+    }
+    public static IEnumerable<Difficulty> GetAllDifficulties() =>
+        Enum.GetValues<Difficulty>();
+}
