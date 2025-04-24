12a7d3da179bee22b9bbe7ec4b994c990cf0f573
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 11:37:13 +0200

    add SudokuSolver for efficient sudoku solving with randomized number order

---
+++ SudokuGenerator/Solvers/SudokuSolver.cs
@@ -0,0 +1,49 @@
+﻿using SudokuGenerator.Validators;
+
+namespace SudokuGenerator.Solvers;
+
+public class SudokuSolver : ISudokuSolver
+{
+    private readonly ISudokuValidator _validator;
+    private readonly Random _rand;
+
+    public SudokuSolver(ISudokuValidator validator, Random rand)
+    {
+        _validator = validator;
+        _rand = rand;
+    }
+
+    public bool Solve(int[,] board) =>
+        Solve(0, 0, board);
+
+    private bool Solve(int row, int col, int[,] board)
+    {
+        if (row == 9)
+            return true;
+        else if (col == 9)
+            return Solve(row + 1, 0, board);
+        else if (board[row, col] != 0)
+            return Solve(row, col + 1, board);
+        else
+        {
+            var possibleNumbers = Enumerable.Range(1, 9).ToList();
+            while (possibleNumbers.Any())
+            {
+                int index = _rand.Next(possibleNumbers.Count());
+                int numberToTest = possibleNumbers[index];
+                possibleNumbers.RemoveAt(index);
+                board[row, col] = numberToTest;
+                if (_validator.Validate(row, col, board))
+                {
+                    if (Solve(row, col + 1, board))
+                        return true;
+                }
+                board[row, col] = 0;
+            }
+
+            return false;
+        }
+    }
+}
+
+
