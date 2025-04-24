87116bc8aca97024176314eaf5ac033bb670647c
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 20:28:17 +0200

    add IRemovalPatternsFactory to abstract IRemovalPatterns creation

---
+++ SudokuGenerator/Factories/IRemovalPatternsFactory.cs
@@ -0,0 +1,9 @@
+﻿using SudokuGenerator.Models;
+using SudokuGenerator.RemovalPatterns;
+
+namespace Sudoku_Generator.Factories;
+
+public interface IRemovalPatternsFactory
+{
+    IList<IRemovalPattern> GetRemovalPatternsFor(Difficulty difficulty);
+}
