0925eab66dcc8e2a757ce6f80b259c48003848d0
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 20:27:28 +0200

    add RemovalPatternsFactory for easier creation of IRemovalPatterns based on selected difficulty

---
+++ SudokuGenerator/Factories/RemovalPatternsFactory.cs
@@ -0,0 +1,49 @@
+﻿using SudokuGenerator.Models;
+using SudokuGenerator.RemovalPatterns;
+using SudokuGenerator.Solvers;
+
+namespace Sudoku_Generator.Factories;
+
+public class RemovalPatternsFactory : IRemovalPatternsFactory
+{
+    private readonly Random _rand;
+    private readonly ISudokuSolver _solver;
+    private readonly Dictionary<Difficulty, IList<IRemovalPattern>> _difficultyToRemovalMapper;
+
+    public RemovalPatternsFactory(Random rand, ISudokuSolver solver)
+    {
+        _rand = rand;
+        _solver = solver;
+        _difficultyToRemovalMapper = new Dictionary<Difficulty, IList<IRemovalPattern>>
+        {
+            [Difficulty.Easy] = new List<IRemovalPattern>
+            {
+                new CheckboardPatternRemoval(rand),
+                new RandomRemovalPattern(solver, rand, Difficulty.Easy)
+            },
+            [Difficulty.Medium] = new List<IRemovalPattern>
+            {
+                new RandomRemovalPattern(solver, rand, Difficulty.Medium)
+            },
+            [Difficulty.Hard] = new List<IRemovalPattern>
+            {
+                new RandomRemovalPattern(solver, rand, Difficulty.Hard)
+            }
+        };
+    }
+
+    public RemovalPatternsFactory(Random rand, ISudokuSolver solver,
+        Dictionary<Difficulty, IList<IRemovalPattern>> difficultyToRemovalMapper)
+    {
+        _rand = rand;
+        _solver = solver;
+        _difficultyToRemovalMapper = difficultyToRemovalMapper;
+    }
+
+    public IList<IRemovalPattern> GetRemovalPatternsFor(Difficulty difficulty)
+    {
+        if (!_difficultyToRemovalMapper.TryGetValue(difficulty, out var patterns))
+            throw new ArgumentException($"No removal patterns defined for difficulty: {difficulty}");
+        return patterns;
+    }
+}
