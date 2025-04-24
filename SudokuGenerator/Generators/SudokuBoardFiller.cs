6460c0a8bae482cd163b17a62ddd1432a0c9afa2
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 11:43:15 +0200

    add SudokuBoardFiller for generating fully solved sudoku boards

---
+++ SudokuGenerator/Generators/SudokuBoardFiller.cs
@@ -0,0 +1,25 @@
+﻿using SudokuGenerator.Solvers;
+
+namespace SudokuGenerator.Generators;
+
+public class SudokuBoardFiller : ISudokuBoardFiller
+{
+    private readonly ISudokuSolver _solver;
+
+    public SudokuBoardFiller(ISudokuSolver solver)
+    {
+        _solver = solver;
+    }
+
+    public int[,] GenerateValidSudokuGrid()
+    {
+        int[,] sudokuBoard;
+        do
+        {
+            sudokuBoard = new int[9, 9];
+        } while (!_solver.Solve(sudokuBoard));
+        return sudokuBoard;
+    }
+}
+
+
