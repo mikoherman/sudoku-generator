06926250fc4f1178562bbbb2fd9f0fca9a12cfe1
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 21:58:44 +0200

    refactor SudokuGeneratorFactory to use LoopingStatusPrinter as a subscriber to the generator's ProcessFinished event

--- SudokuGenerator/Factories/SudokuGeneratorFactory.cs
+++ SudokuGenerator/Factories/SudokuGeneratorFactory.cs
@@ -2,6 +2,7 @@
 using SudokuGenerator.RemovalPatterns;
 using Generators = SudokuGenerator.Generators;
 using SudokuGenerator.Generators;
+using Sudoku_Generator.UserInteraction;
 
 namespace Sudoku_Generator.Factories;
 
@@ -9,12 +10,14 @@ public class SudokuGeneratorFactory : ISudokuGeneratorFactory
 {
     private readonly ISudokuBoardFiller _boardFiller;
     private readonly IRemovalPatternsFactory _removalPatternsFactory;
+    private readonly ILoopingStatusPrinter _loopingStatusPrinter;
     private readonly Random _rand;
 
-    public SudokuGeneratorFactory(ISudokuBoardFiller boardFiller, IRemovalPatternsFactory removalPatternsFactory, Random rand)
+    public SudokuGeneratorFactory(ISudokuBoardFiller boardFiller, IRemovalPatternsFactory removalPatternsFactory, ILoopingStatusPrinter loopingStatusPrinter, Random rand)
     {
         _boardFiller = boardFiller;
         _removalPatternsFactory = removalPatternsFactory;
+        _loopingStatusPrinter = loopingStatusPrinter;
         _rand = rand;
     }
 
@@ -22,6 +25,7 @@ public class SudokuGeneratorFactory : ISudokuGeneratorFactory
     {
         IList<IRemovalPattern> removalPatterns = _removalPatternsFactory.GetRemovalPatternsFor(difficulty);
         var generator = new Generators.SudokuGenerator(_boardFiller, removalPatterns, _rand);
+        generator.ProcessFinished += _loopingStatusPrinter.FinishWork;
         return generator;
     }
 }
