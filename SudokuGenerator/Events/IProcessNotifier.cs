1f30b7cfaed7e159f241f731bd865c709cdb62d3
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 20:49:58 +0200

    add IProcessNotifier to abstract ProcessFinished event

---
+++ SudokuGenerator/Events/IProcessNotifier.cs
@@ -0,0 +1,9 @@
+﻿namespace Sudoku_Generator.Events;
+
+public interface IProcessNotifier
+{
+    /// <summary>
+    /// Raised when the process has finished its work.
+    /// </summary>
+    event EventHandler<IsProcessFinishedEventArgs>? ProcessFinished;
+}
