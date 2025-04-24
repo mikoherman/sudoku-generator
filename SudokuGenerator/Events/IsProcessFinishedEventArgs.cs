86645bdda5c652a67e8b06afe9db6f4d03426082
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 20:41:06 +0200

    add IsProcessFinishedEventArgs to desribe data passed in the event

---
+++ SudokuGenerator/Events/IsProcessFinishedEventArgs.cs
@@ -0,0 +1,9 @@
+﻿namespace Sudoku_Generator.Events;
+
+public class IsProcessFinishedEventArgs : EventArgs {
+    public bool IsProcessFinished { get; }
+    public IsProcessFinishedEventArgs(bool isProcessFinished)
+    {
+        IsProcessFinished = isProcessFinished;
+    }
+}
