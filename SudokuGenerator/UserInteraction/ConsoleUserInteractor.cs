043e419eeb34d52466e10dbe5614ac3f59836080
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-24 12:23:18 +0200

    add ConsoleUserInteractor for reading from and writing to console

---
+++ SudokuGenerator/UserInteraction/ConsoleUserInteractor.cs
@@ -0,0 +1,14 @@
+﻿namespace SudokuGenerator.UserInteraction;
+
+public class ConsoleUserInteractor : IConsoleUserInteractor
+{
+    public string? Read()
+    {
+        return Console.ReadLine();
+    }
+
+    public void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
